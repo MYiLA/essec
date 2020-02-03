dbc35966df961f49f1b1be27513095f3ab64f620
MYiLA <polus180@ya.ru>
2020-02-03 16:43:59 +0300

    css for preview

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const tabletWidth = 900;
-
+//- script(type='text/javascript' defer) new Glide('.glide', {type: 'carousel',startAt: 0, perView: 3, gap: 30, autoplay: 4000, animationTimingFunc: 'cubic-bezier(.36,.39,.31,1.02)', dragThreshold: 50, animationDuration: 1000, breakpoints: { 1100: {perView: 2 },550: {perView: 1}},}).mount();
 // взятие значения из адресной строки(запросы по ссылкам)
 const getUrlParams = search => {
   const params = {};
