e1dd1e884c7f048d43f58784af4cc34eb503f87f
MYiLA <polus180@ya.ru>
2020-02-05 02:05:31 +0300

    add js

--- source/js/script.js
+++ source/js/script.js
@@ -1,44 +1,8 @@
 'use strict';
 
 const tabletWidth = 900;
-//- script(type='text/javascript' defer) new Glide('.glide', {type: 'carousel',startAt: 0, perView: 3, gap: 30, autoplay: 4000, animationTimingFunc: 'cubic-bezier(.36,.39,.31,1.02)', dragThreshold: 50, animationDuration: 1000, breakpoints: { 1100: {perView: 2 },550: {perView: 1}},}).mount();
-// взятие значения из адресной строки(запросы по ссылкам)
-const getUrlParams = search => {
-  const params = {};
-  search.substr(1).split("&").forEach(part => {
-    const [key, value] = part.split("=");
-    params[key] = decodeURIComponent(value);
-  });
-  return params;
-};
 
-let requestUrl = getUrlParams(window.location.search).wheel || 'дефолтный запрос';
-
-// // запросы Url (настройка ссылок)
-// const openBookmark = () => {
-//   if (requestUrl === "superieur") {
-//     функция, которую нужно выполнить по запросу superieur
-//   }
-
-//   if (requestUrl === "etrangere") {
-//     функция, которую нужно выполнить по запросу etrangere
-//   } else {
-//     $('.main-nav__bookmark-item--superieur main-nav__bookmark').trigger('click');
-//   }
-// }
-// openBookmark();
-
-// Плавный скролл до дом-элемента
-
-$(document).ready(function () {
-  $('a').on('click', function (event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-
-      $('html, body').animate({
-        scrollTop: ($(hash).offset().top) - 150 // 150px - высота липкой шапки(докрутка)
-      }, 800);
-    }
-  });
-});
+const closeAnimation = function (wrap) {
+  wrap.classList.remove('dissolve-show');
+  wrap.classList.add('dissolve-hidden');
+}
